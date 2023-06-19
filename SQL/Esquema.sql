816e1fe65328151736fbd95b6a10776891bca7c3
renatoTheoJr <renatotheojunior@gmail.com>
2023-06-19 11:56:36 -0300

    Criação das tabelas atualizado

--- SQL/Esquema.sql
+++ SQL/Esquema.sql
@@ -3,8 +3,8 @@ CREATE TABLE Deficiencia (
     nome VARCHAR2(30) NOT NULL,
     tipo VARCHAR2(15) NOT NULL,
 
-    CONSTRAINT pk_deficiencia PRIMARY KEY cid,
-    CONSTRAINT ck_tipo CHECK (tipo IN ('VISUAL', 'AUDITIVA', 'FÃSICA', 'INTELECTUAL', 'PSICOSOCIAL'))
+    CONSTRAINT pk_deficiencia PRIMARY KEY (cid),
+    CONSTRAINT ck_tipo CHECK (tipo IN ('VISUAL', 'AUDITIVA', 'FÍSICA', 'INTELECTUAL', 'PSICOSOCIAL'))
 );
 
 CREATE TABLE Escola (
@@ -20,7 +20,7 @@ CREATE TABLE Escola (
     telefone_fixo CHAR(10),
     telefone_celular CHAR(11),
 
-    CONSTRAINT pk_escola PRIMARY KEY cnpj,
+    CONSTRAINT pk_escola PRIMARY KEY (cnpj),
     CONSTRAINT ck_natureza CHECK (natureza IN ('PUBLICA', 'PRIVADA'))
 );
 
@@ -30,8 +30,8 @@ CREATE TABLE Curso (
     descricao VARCHAR2(120),
     deficiencia CHAR(3) NOT NULL,
 
-    CONSTRAINT pk_curso PRIMARY KEY nome,
-    CONSTRAINT fk_curso_deficiencia FOREIGN KEY deficiencia REFERENCES Deficiencia(cid)
+    CONSTRAINT pk_curso PRIMARY KEY (nome),
+    CONSTRAINT fk_curso_deficiencia FOREIGN KEY (deficiencia) REFERENCES Deficiencia(cid),
     CONSTRAINT ck_duracao CHECK (duracao > 0)
 );
 
@@ -39,7 +39,7 @@ CREATE TABLE Vinculo (
     cpf CHAR(11) NOT NULL,
     vinculo VARCHAR2(10) NOT NULL,
 
-    CONSTRAINT pk_vinculo PRIMARY KEY cpf,
+    CONSTRAINT pk_vinculo PRIMARY KEY (cpf),
     CONSTRAINT ck_vinculo CHECK (vinculo IN ('PROFESSOR', 'ASSISTENTE'))
 );
 
@@ -55,7 +55,7 @@ CREATE TABLE Professor (
     telefone_celular CHAR(11),
     formacao VARCHAR2(30),
 
-    CONSTRAINT pk_professor PRIMARY KEY cpf,
+    CONSTRAINT pk_professor PRIMARY KEY (cpf)
 );
 
 CREATE TABLE Assistente (
@@ -69,7 +69,7 @@ CREATE TABLE Assistente (
     telefone_fixo CHAR(10),
     telefone_celular CHAR(11),
 
-    CONSTRAINT pk_assistente PRIMARY KEY cpf,
+    CONSTRAINT pk_assistente PRIMARY KEY (cpf)
 );
 
 CREATE TABLE Oferecimento (
@@ -81,10 +81,11 @@ CREATE TABLE Oferecimento (
     data_inicio DATE NOT NULL,
     data_fim DATE NOT NULL,
 
-    CONSTRAINT pk_oferecimento PRIMARY KEY num_contrato,
-    CONSTRAINT fk_oferecimento_curso FOREIGN KEY curso REFERENCES Curso(nome),
-    CONSTRAINT fk_oferecimento_escola FOREIGN KEY escola REFERENCES Escola(cnpj),
-    CONSTRAINT fk_oferecimento_professor FOREIGN KEY professor REFERENCES Professor(cpf)
+    CONSTRAINT pk_oferecimento PRIMARY KEY (num_contrato),
+    CONSTRAINT fk_oferecimento_curso FOREIGN KEY (curso) REFERENCES Curso(nome),
+    CONSTRAINT fk_oferecimento_escola FOREIGN KEY (escola) REFERENCES Escola(cnpj),
+    CONSTRAINT fk_oferecimento_professor FOREIGN KEY (professor) REFERENCES Professor(cpf),
+    CONSTRAINT ck_oferecimento_doacao CHECK(doacao >= 0)
 );
 
 CREATE TABLE Aluno (
@@ -96,8 +97,8 @@ CREATE TABLE Aluno (
     telefone_fixo CHAR(10),
     telefone_celular CHAR(11),
 
-    CONSTRAINT pk_aluno PRIMARY KEY cpf,
-    CONSTRAINT fk_aluno_deficiencia FOREIGN KEY deficiencia REFERENCES Deficiencia(cid),
+    CONSTRAINT pk_aluno PRIMARY KEY (cpf),
+    CONSTRAINT fk_aluno_deficiencia FOREIGN KEY (deficiencia) REFERENCES Deficiencia(cid),
     CONSTRAINT ck_grau CHECK (grau IN ('A', 'B', 'C', 'D', 'E')) 
 );
 
@@ -109,9 +110,9 @@ CREATE TABLE Aula (
     sitio VARCHAR2(30) NOT NULL,
     numeracao CHAR(2) NOT NULL,
 
-    CONSTRAINT pk_aula PRIMARY KEY id,
+    CONSTRAINT pk_aula PRIMARY KEY (id),
     CONSTRAINT un_oferecimento_data UNIQUE (oferecimento, data_hora_inicio),
-    CONSTRAINT fk_aula_oferecimento FOREIGN KEY oferecimento REFERENCES Oferecimento(num_contrato)
+    CONSTRAINT fk_aula_oferecimento FOREIGN KEY (oferecimento) REFERENCES Oferecimento(num_contrato)
 );
 
 
@@ -121,9 +122,9 @@ CREATE TABLE Presenca (
     assistente CHAR(11) NOT NULL,
 
     CONSTRAINT pk_presenca PRIMARY KEY (aluno, aula),
-    CONSTRAINT fk_presenca_aluno FOREIGN KEY aluno REFERENCES Aluno(cpf),
-    CONSTRAINT fk_presenca_aula FOREIGN KEY aula REFERENCES Aula(id),
-    CONSTRAINT fk_presenca_assistente FOREIGN KEY assistente REFERENCES Assistente(cpf)
+    CONSTRAINT fk_presenca_aluno FOREIGN KEY (aluno) REFERENCES Aluno(cpf),
+    CONSTRAINT fk_presenca_aula FOREIGN KEY (aula) REFERENCES Aula(id),
+    CONSTRAINT fk_presenca_assistente FOREIGN KEY (assistente) REFERENCES Assistente(cpf)
 );
 
 CREATE TABLE Recurso (
@@ -131,7 +132,7 @@ CREATE TABLE Recurso (
     tipo VARCHAR2(30) NOT NULL,
     nome VARCHAR2(30) NOT NULL,
 
-    CONSTRAINT pk_recurso PRIMARY KEY num_patrimonio
+    CONSTRAINT pk_recurso PRIMARY KEY (num_patrimonio)
 );
 
 CREATE TABLE Aula_Recurso (
@@ -139,7 +140,7 @@ CREATE TABLE Aula_Recurso (
     recurso CHAR(10) NOT NULL,
 
     CONSTRAINT pk_aula_recurso PRIMARY KEY (aula, recurso),
-    CONSTRAINT fk_aula_recurso_aula FOREIGN KEY aula REFERENCES Aula(id),
-    CONSTRAINT fk_aula_recurso_recurso FOREIGN KEY recurso REFERENCES Recurso(num_patrimonio)
+    CONSTRAINT fk_aula_recurso_aula FOREIGN KEY (aula) REFERENCES Aula(id),
+    CONSTRAINT fk_aula_recurso_recurso FOREIGN KEY (recurso) REFERENCES Recurso(num_patrimonio)
 );
 
