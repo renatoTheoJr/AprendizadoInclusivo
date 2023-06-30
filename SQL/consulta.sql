3f0f757258c0644f53cecc36bf4ecc84c14ac4d8
Matheus Giraldi Alvarenga <62122159+matheusgiraldi@users.noreply.github.com>
2023-06-30 00:51:40 -0300

    Final queries added and other minor adjustments

--- SQL/consulta.sql
+++ SQL/consulta.sql
@@ -1,62 +1,85 @@
  -- Lista todos os alunos que não faltaram nenhuma aula do contrato CON0000001
-
-select a.nome from aluno a where
-not exists((
-    select id from aula au where
+SELECT A.nome FROM aluno A WHERE
+NOT EXISTS((
+    SELECT id FROM aula au WHERE
     au.oferecimento = 'CON0000001')
     minus
     (
-        select p.aula from presenca p where
-        p.aluno = a.cpf
+        SELECT P.aula FROM presenca P WHERE
+        P.aluno = A.cpf
     )
 )
 
 -- Lista todos os alunos que frequentaram pelo menos uma aula de todos ofereciementos do professor 26576719000
-select a.nome from aluno a where
-not exists((
-    select o.num_contrato from oferecimento o where
-    o.professor = '48425678056')
+SELECT A.nome FROM aluno A WHERE
+NOT EXISTS((
+    SELECT O.num_contrato FROM oferecimento O WHERE
+    O.professor = '48425678056')
     minus
     (
-        select au.oferecimento from aula au where
-        au.id in (select p.aula from presenca p where
-        p.aluno = a.cpf)
+        SELECT au.oferecimento FROM aula au WHERE
+        au.id IN (SELECT P.aula FROM presenca P WHERE
+        P.aluno = A.cpf)
     )
 ) 
 
 -- Listar deficiências que são abrangidas por pelo menos 3 cursos
-select d.cid from deficiencia d inner join curso c on c.deficiencia = d.cid
-group by d.cid
-having count(*) >=3
+SELECT d.cid FROM deficiencia d INNER JOIN curso c ON c.deficiencia = d.cid
+GROUP BY d.cid
+HAVING COUNT(*) >=3
 
 
 -- Listar todos os oferencimentos que não tiveram nenhuma aula
-select o.num_contrato from oferecimento o left join aula a on a.oferecimento = o.num_contrato
-where a.id is null
+SELECT O.num_contrato FROM oferecimento O left join aula A ON A.oferecimento = O.num_contrato
+WHERE A.id IS NULL
 
 
--- Listar todas as escolas que o tempo de duração do contrato é maior que 1 ano
-select e.nome, o.num_contrato from escola e inner join oferecimento o on o.escola = e.cnpj
-where o.data_fim - o.data_inicio > 365
+-- Listar todas AS escolas que O tempo de duração do contrato é maior que 1 ano
+SELECT E.nome, O.num_contrato FROM escola E INNER JOIN oferecimento O ON O.escola = E.cnpj
+WHERE O.data_fim - O.data_inicio > 365
 
--- Listar todas as escolas que o tempo de contrato já encerrou
-select e.nome, o.num_contrato from escola e inner join oferecimento o on o.escola = e.cnpj
-where o.data_fim < sysdate 
+-- Listar todas AS escolas que O tempo de contrato já encerrou
+SELECT E.nome, O.num_contrato FROM escola E INNER JOIN oferecimento O ON O.escola = E.cnpj
+WHERE O.data_fim < SYSDATE 
 
 
 -- Listar quantos contratos ativos cada escola tem
-select e.nome, count(*) as contrato_ativo from escola e inner join oferecimento o on o.escola = e.cnpj
-where o.data_fim > sysdate
-group by e.nome
+SELECT E.nome, COUNT(*) AS contrato_ativo FROM escola E INNER JOIN oferecimento O ON O.escola = E.cnpj
+WHERE O.data_fim > SYSDATE
+GROUP BY E.nome
 
 --Listar nome de escolas em que um professor da aula atualmente
-select e.nome as escola, p.nome as professor from escola e inner join oferecimento o on o.escola = e.cnpj and o.data_fim > sysdate 
-inner join professor p on p.cpf = o.professor
+SELECT E.nome AS escola, P.nome AS professor FROM escola E INNER JOIN oferecimento O ON O.escola = E.cnpj AND O.data_fim > SYSDATE 
+INNER JOIN professor P ON P.cpf = O.professor
 
 
 -- Listar deficiências que não são abrangidas por nenhum curso
-select d.cid from deficiencia d left join curso c on c.deficiencia = d.cid
-where c.cid is null
+SELECT d.cid FROM deficiencia d left join curso c ON c.deficiencia = d.cid
+WHERE c.cid IS NULL
 
--- Listar deficiências e se tiver seus respectivos cursos
-select d.cid, c.nome from deficiencia d left join curso c on c.deficiencia = d.cid
+-- Listar deficiências E se tiver seus respectivos cursos
+SELECT d.cid, c.nome FROM deficiencia d left join curso c ON c.deficiencia = d.cid
+
+-- Listar todas AS aulas oferecidas em 2020 que usaram mais de dois recursos:
+SELECT A.id, A.oferecimento, A.data_hora_inicio, A.data_hora_fim, A.sitio, A.numeracao
+FROM Aula A
+INNER JOIN Oferecimento O ON A.oferecimento = O.num_contrato
+INNER JOIN Aula_Recurso AR ON A.id = AR.aula
+INNER JOIN Recurso R ON AR.recurso = R.num_patrimonio
+WHERE EXTRACT(YEAR FROM A.data_hora_inicio) = 2020
+GROUP BY A.id, A.oferecimento, A.data_hora_inicio, A.data_hora_fim, A.sitio, A.numeracao
+HAVING COUNT(R.num_patrimonio) > 2;
+
+-- Listar os alunos matriculados em um determinado curso:
+SELECT A.cpf, A.deficiencia, A.grau, A.nome, A.atestado, A.telefone_fixo, A.telefone_celular
+FROM Aluno A
+INNER JOIN Curso C ON A.deficiencia = C.deficiencia
+WHERE C.nome = 'Introdução à Informática';
+
+-- Listar os recursos que um oferecimento usa:
+SELECT R.num_patrimonio, R.tipo, R.nome
+FROM Recurso R
+INNER JOIN Aula_Recurso AR ON R.num_patrimonio = AR.recurso
+INNER JOIN Aula A ON AR.aula = A.id
+INNER JOIN Oferecimento O ON A.oferecimento = O.num_contrato
+WHERE O.num_contrato = 'CON0000001';
